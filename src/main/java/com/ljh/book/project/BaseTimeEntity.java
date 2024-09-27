5ffd3aeb764d344d75c22dc9fb22c3b3cc7c0c07
DESKTOP-MST6EOC\ksna0303 <ksna0303@gmail.com>
2024-09-27 22:06:30 +0900

    Transaction Test

--- src/main/java/com/ljh/book/project/BaseTimeEntity.java
+++ src/main/java/com/ljh/book/project/BaseTimeEntity.java
@@ -17,6 +17,7 @@ public abstract class BaseTimeEntity {
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdDate;
+
     @LastModifiedDate
     private LocalDateTime modifiedDate;
 }
